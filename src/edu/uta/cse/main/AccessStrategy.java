14d57a75d5b2afb0220afa42cde017a56480b0f2
zhangboryan <zhangbo@host-217-99.txarmav.clients.pavlovmedia.com>
2014-03-24 00:10:09 -0500

    change parameter of doAnalysis

--- src/edu/uta/cse/main/AccessStrategy.java
+++ src/edu/uta/cse/main/AccessStrategy.java
@@ -3,7 +3,7 @@ package edu.uta.cse.main;
 public class AccessStrategy extends CodeHandlerStrategy{
 
 	@Override
-	public String[] doAnalysis(String selectedWord, String contex) {
+	public String[] doAnalysis(CodeFileReader cfr, String contex, String selectedWord) {
 		// TODO Auto-generated method stub
 		return "".split("");
 	}
--- src/edu/uta/cse/main/VariableStrategy.java
+++ src/edu/uta/cse/main/VariableStrategy.java
@@ -3,7 +3,7 @@ package edu.uta.cse.main;
 public class VariableStrategy extends CodeHandlerStrategy{
 
 	@Override
-	public String[] doAnalysis(String selectedWord, String contex) {
+	public String[] doAnalysis(CodeFileReader cfr, String contex, String selectedWord) {
 
 		
 		
